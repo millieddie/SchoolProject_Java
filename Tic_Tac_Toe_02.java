5fb3c770d0bd1fda0587185205d1bef52a083e7e
Akira M <133370976+millieddie@users.noreply.github.com>
2024-01-15 15:24:20 +0900

    Update Tic_Tac_Toe_02.java

--- Tic_Tac_Toe_02.java
+++ Tic_Tac_Toe_02.java
@@ -4,7 +4,7 @@ public class Tic_Tac_Toe_02 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO 自動生成されたメソッド・スタブ
-		HomeWork_No2_Sub work = new HomeWork_No2_Sub();
+		Tic_Tac_Toe_02_SubClass work = new Tic_Tac_Toe_02_SubClass();
 		
 		//表を出す
 		work.Rows();
